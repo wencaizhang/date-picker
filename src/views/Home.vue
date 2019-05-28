072160ac81f5e76595a71c95b55cc46df39182dc
wencaizhang <1052642137@qq.com>
2019-05-28 11:28:03 +0800

    update display

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
     <button @click="visible = !visible">选择时间！</button>
-
+    <br>
+    <div style="text-align: left; display: inline-block; margin: 10px auto;">
+      <p>已选择的时间：</p>
+      <p>时间：{{ time[0] || '无' }}</p>
+      <p>时间戳：{{ time[1] || '无' }}</p>
+    </div>
     <picker :visible.sync="visible" @confirm="onConfirm" :during="10" :interval="20"/>
   </div>
 </template>
@@ -17,12 +22,14 @@ export default {
   },
   data() {
     return {
-      visible: false
+      visible: false,
+      time: []
     };
   },
   methods: {
     onConfirm(time, timestamp) {
-      console.log([].slice.apply(arguments));
+      this.time = [].slice.apply(arguments);
+      console.log(this.time);
     }
   }
 };
